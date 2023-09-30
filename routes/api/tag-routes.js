faa82eeaf8efd85e952bf483f0d01b12f820b8fb
alliedeaver <adeav97@gmail.com>
2023-09-29 23:17:03 -0700

    added tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,9 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+
+
+
 });
 
 router.get('/:id', (req, res) => {
@@ -19,6 +22,25 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
+  Tag.update(
+    { 
+      tag_name: req.body.name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((updatedTag) => {
+    res.json(updatedTag);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
