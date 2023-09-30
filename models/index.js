f5ffb48b3ac78b789ed7d973446852adfd385e2f
alliedeaver <adeav97@gmail.com>
2023-09-29 23:05:10 -0700

    added to the indexjs

--- models/index.js
+++ models/index.js
@@ -6,11 +6,19 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category);
+
 // Categories have many Products
+
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongToMany(Tag, {through: ProductTag});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongToMany(Product, {through: ProductTag});
 
 module.exports = {
   Product,
